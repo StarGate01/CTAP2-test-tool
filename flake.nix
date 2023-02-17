073ba2f3b1dc3860b06cd1fcf0525271a6693282
Christoph Honal <christoph.honal@web.de>
2023-02-17 01:27:10 +0100

    Fix build on NixOS

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in
+    rec {
+      devShell.x86_64-linux = (pkgs.buildFHSUserEnv {
+         name = "bazel-env";
+         targetPkgs = pkgs: with pkgs; [
+              gcc
+              pkg-config
+              bazel
+              systemd.dev
+              automake
+              autoconf
+              autogen
+              libtool
+           ];
+       }).env;
+    };
+}
